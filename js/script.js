f860025ab3a25248af9cfde7bcf35c232ef6912d
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2025-07-11 17:23:26 -0300

    updating

--- js/script.js
+++ js/script.js
@@ -4,13 +4,98 @@ let img = document.querySelector("#img");
 let addtaskInput = document.querySelector("#add-task-input");
 let addtaskBtn = document.querySelector("#add-task");
 let searchInput = document.querySelector("#searchInput");
-let taskDescription = document.querySelector("#task-description");
 let tasksContainer = document.querySelector(".tasks");
+let filter = document.querySelector("#filter");
+let imgEmpty = document.querySelector(".empty");
 
 let popUp = document.querySelector("#popup");
 let cancelBtn = document.querySelector("#cancel-popup");
 
+// Functions
+
 const cancelPopUP = () => popUp.classList.toggle("hidden");
+
+const abrirPopUp = () => popUp.classList.toggle("hidden");
+
+const filtrar = (filterValue) => {
+  const todos = document.querySelectorAll(".task");
+
+  switch (filterValue) {
+    case "all":
+      todos.forEach((todo) => todo.classList.remove("hide"));
+      break;
+    case "complete":
+      todos.forEach((todo) => {
+        todo.classList.contains("complete")
+          ? todo.classList.remove("hide")
+          : todo.classList.toggle("hide");
+      });
+      break;
+    case "incomplete":
+      todos.forEach((todo) => {
+        !todo.classList.contains("complete")
+          ? todo.classList.remove("hide")
+          : todo.classList.toggle("hide");
+      });
+      break;
+    default:
+      break;
+  }
+};
+
+
+const createTask = (span) => {
+  imgEmpty.classList.add("hidden");
+
+  const divTask = document.createElement("div");
+  divTask.classList.add("task");
+  tasksContainer.appendChild(divTask);
+
+  const taskTitle = document.createElement("div");
+  taskTitle.classList.add("task-title");
+  divTask.appendChild(taskTitle);
+
+  let title = document.createElement("span");
+  title.textContent = span;
+  taskTitle.appendChild(title);
+
+  const options = document.createElement("div");
+  options.classList.add("task-options");
+  divTask.appendChild(options);
+
+  const btn = document.createElement("button");
+  const imgCheck = document.createElement("img");
+  imgCheck.src = "images/check.svg";
+  imgCheck.classList.add("finish");
+  options.appendChild(btn);
+  btn.appendChild(imgCheck);
+
+  imgCheck.addEventListener("click", () => {
+    divTask.classList.toggle("complete");
+  });
+
+  const btn3 = document.createElement("button");
+  const imgEdit = document.createElement("img");
+  imgEdit.src = "images/edit.svg";
+  imgEdit.classList.add("edit");
+  btn3.appendChild(imgEdit);
+  options.appendChild(btn3);
+
+  const btn2 = document.createElement("button");
+  const imgDelete = document.createElement("img");
+  imgDelete.src = "images/trash.svg";
+  imgDelete.classList.add("remove");
+  btn2.appendChild(imgDelete);
+  options.appendChild(btn2);
+
+  imgDelete.addEventListener("click", () => {
+    divTask.remove();
+  });
+
+  addtaskInput.value = "";
+};
+
+// Events
 
 cancelBtn.addEventListener("click", () => {
   cancelPopUP();
@@ -26,62 +111,31 @@ button.addEventListener("click", () => {
   }
 });
 
-const createTask = () => {
-  const divTask = document.createElement("div");
-  divTask.classList.add("task");
-
-  const divTaskHeader = document.createElement("div");
-  divTaskHeader.classList.add("task-header");
-
-  const taskTitle = document.createElement("div");
-  taskTitle.classList.add("task-title");
-
-  let title = document.createElement("span");
-  title.textContent = addtaskInput.value;
-
-  const options = document.createElement("div");
-  options.classList.add("task-options");
-
-  const btn = document.createElement("button");
-  const imgCheck = document.createElement("img");
-  imgCheck.src = "images/check.svg";
-
-  imgCheck.addEventListener("click", () => {
-    divTask.classList.toggle("complete");
-  });
-
-  const btn2 = document.createElement("button");
-  const imgDelete = document.createElement("img");
-  imgDelete.src = "images/trash.svg";
-
-  const description = document.createElement("div");
-  description.classList.add("task-description");
-  const descriptionParagraphy = document.createElement("p");
-
-  descriptionParagraphy.textContent = taskDescription.value;
-
-  description.appendChild(descriptionParagraphy);
-
-  tasksContainer.appendChild(divTask);
-  divTask.appendChild(divTaskHeader);
-  divTask.appendChild(description);
-  divTaskHeader.appendChild(taskTitle);
-  divTaskHeader.appendChild(options);
-  taskTitle.appendChild(title);
-
-  options.appendChild(btn);
-  options.appendChild(btn2);
-  btn.appendChild(imgCheck);
-  btn2.appendChild(imgDelete);
-};
+filter.addEventListener("change", (e) => {
+  let filterValue = e.target.value;
+  filtrar(filterValue);
+});
 
 addtaskBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if (!addtaskInput.value == "") {
-    createTask();
+  let inputValue = addtaskInput.value.trim();
+
+  if (inputValue) {
+    createTask(inputValue);
     addtaskInput.value = "";
-    taskDescription.value = "";
+    cancelPopUP();
+  }
+});
+
+addtaskInput.addEventListener("keyup", (e) => {
+  e.preventDefault();
+
+  let inputValue = addtaskInput.value.trim();
+
+  if (e.key === "Enter" && !addtaskInput.value == "") {
+    createTask(inputValue);
+    addtaskInput.value = "";
     cancelPopUP();
   }
 });
